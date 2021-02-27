6bb6a7e3b089c823ec2e54715b76219a02240707
Rocket City Dynamics <46632364+RocketCityDynamics@users.noreply.github.com>
2021-02-26 19:26:33 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ class MycroftRoxyTurtlesimTurtleForward(MycroftSkill):
 
     @intent_file_handler('forward.turtle.turtlesim.roxy.mycroft.intent')
     def handle_forward_turtle_turtlesim_roxy_mycroft(self, message):
-    self.speak_dialog('forward.turtle.turtlesim.roxy.mycroft')
-    subprocess.call(["ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist '{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}'"],shell=True)
+        self.speak_dialog('forward.turtle.turtlesim.roxy.mycroft')
+        subprocess.call(["ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist '{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}'"],shell=True)
 
 
 def create_skill():
